56f58a3f9b1b075a7bfd1bf9af780035510f64a1
Machine <ezoneid@gmail.com>
2016-12-09 17:21:35 -0600

    Remove Canvas dev on server

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ const bodyParser = require('body-parser');
 
 const wordFilter = require('./wordFilter.js');
 
-const Canvas = require("canvas");
-
 const cloud = require("./public/js/d3.layout.cloud.js");
 
 
